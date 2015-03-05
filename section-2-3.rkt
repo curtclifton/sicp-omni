d11c8dc7058de7d9674e8f38020f77f34da9ec90
Curt Clifton <curt.clifton@mac.com>
2015-03-04 21:55:19 -0800

    Exercise 2.56, plus the given code from section 2.3.2.

--- section-2-3.rkt
+++ section-2-3.rkt
@@ -45,3 +45,91 @@
 (car ''abracadabra)
 
 ; Expanding the single quotes per footnote 34 (sheesh), this is equivalent to (car (quote (quote abracadabra))), which is equivalent to (car (list 'quote 'abracadabra)).
+
+;;; Section 2.3.2
+
+(println "Section 2.3.2")
+
+(define (=number? exp num)
+  (and (number? exp) (= exp num)))
+
+(define (variable? x) (symbol? x))
+(define (same-variable? v1 v2)
+  (and (variable? v1) (variable? v2) (eq? v1 v2)))
+
+(define (make-sum a1 a2)
+  (cond ((=number? a1 0) a2)
+        ((=number? a2 0) a1)
+        ((and (number? a1) (number? a2)) (+ a1 a2))
+        (else (list '+ a1 a2))))
+(define (sum? x)
+  (and (pair? x) (eq? (car x) '+)))
+(define (addend s) (cadr s))
+(define (augend s) (caddr s))
+
+(define (make-product m1 m2)
+  (cond ((or (=number? m1 0) (=number? m2 0)) 0)
+        ((=number? m1 1) m2)
+        ((=number? m2 1) m1)
+        ((and (number? m1) (number? m2)) (* m1 m2))
+        (else (list '* m1 m2))))
+(define (product? x)
+  (and (pair? x) (eq? (car x) '*)))
+(define (multiplier p) (cadr p))
+(define (multiplicand p) (caddr p))
+
+(define (make-power b e)
+  (cond ((=number? e 0) 1)
+        ((=number? e 1) b)
+        (else (list '** b e))))
+(define (power? e)
+  (and (pair? e) (eq? (car e) '**)))
+(define (base p) (cadr p))
+(define (exponent p) (caddr p))
+
+(define (deriv exp var)
+  (cond ((number? exp) 0)
+        ((variable? exp)
+         (if (same-variable? exp var) 1 0))
+        ((sum? exp)
+         (make-sum (deriv (addend exp) var)
+                   (deriv (augend exp) var)))
+        ((product? exp)
+         (make-sum
+           (make-product (multiplier exp)
+                         (deriv (multiplicand exp) var))
+           (make-product (deriv (multiplier exp) var)
+                         (multiplicand exp))))
+        ((power? exp)
+         (make-product (exponent exp)
+                       (make-product (make-power (base exp) 
+                                                 (- (exponent exp) 1))
+                                     (deriv (base exp) var))))
+        (else
+         (error "unknown expression type -- DERIV" exp))))
+
+(deriv '(+ x 3) 'x)
+(deriv '(* x y) 'x)
+(deriv '(* (* x y) (+ x 3)) 'x)
+
+;;; Exercise 2.56
+
+(println "Exercise 2.56")
+
+; See definitions of make-piwer, power?, base, and exponent above, along with the power? case in deriv.
+
+(deriv (make-power 'x 0) 'x)
+(deriv (make-power 'x 1) 'x)
+(deriv (make-power 'x 2) 'x)
+(deriv (make-power 'x 3) 'x)
+
+(define (second-deriv exp var)
+  (deriv (deriv exp var) var))
+
+(second-deriv (make-power 'x 0) 'x)
+(second-deriv (make-power 'x 1) 'x)
+(second-deriv (make-power 'x 2) 'x)
+(second-deriv (make-power 'x 3) 'x)
+
+
+
