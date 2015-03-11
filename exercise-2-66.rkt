d039ce52841bfb980ef3c56ad4feacda6147956d
Curt Clifton <curt.clifton@mac.com>
2015-03-10 21:36:56 -0700

    Some notes on exercise 2.66t

---
+++ exercise-2-66.rkt
@@ -0,0 +1,5 @@
+#lang racket
+
+;;; Exercise 2.66
+
+; Nope. Give us an actual representation of the data. (Essentially this is just element-of? for sets using the binary tree representation, but slap an application of key onto the entry and return the found entry instead of #t.)
