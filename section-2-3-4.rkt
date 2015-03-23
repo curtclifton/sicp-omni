be583cda7ff328a2f70e3d5cc2269ccd7e3cc22c
Curt Clifton <curt.clifton@mac.com>
2015-03-22 17:52:15 -0700

    Through exercise 2.68.

--- section-2-3-4.rkt
+++ section-2-3-4.rkt
@@ -1,5 +1,21 @@
 #lang racket
 
+(define (println . xs)
+  (define (print items)
+    (if (null? items)
+        (newline)
+        (begin
+          (display (car items))
+          (print (cdr items)))))
+  (print xs))
+
+(define (make-leaf symbol weight)
+  (list 'leaf symbol weight))
+(define (leaf? object)
+  (eq? (car object) 'leaf))
+(define (symbol-leaf x) (cadr x))
+(define (weight-leaf x) (caddr x))
+
 (define (make-code-tree left right) (list left
                                           right
                                           (append (symbols left) (symbols right))
@@ -9,7 +25,8 @@
 
 (define (symbols tree)
   (if (leaf? tree)
-      (list (symbol-leaf tree)) (caddr tree)))
+      (list (symbol-leaf tree))
+      (caddr tree)))
 
 (define (weight tree) (if (leaf? tree)
                           (weight-leaf tree)
@@ -49,6 +66,8 @@
 
 ;;;Exercise 2.67
 
+(println "exercise 2.67")
+
 (define sample-tree (make-code-tree (make-leaf 'A 4)
                                     (make-code-tree
                                      (make-leaf 'B 2)
@@ -58,16 +77,39 @@
 
 (define sample-message '(0 1 1 0 0 1 0 1 0 1 1 1 0))
 
+(define decoded-message (decode sample-message sample-tree))
+(println "(decode sample-message sample-tree) ==> " decoded-message)
+
 ;;; Exercise 2.68
+
+(define (encodes-symbol? sym tree)
+  (memq sym (symbols tree)))
+  
+(define (encode-symbol sym tree)
+  (define (walk-subtree subtree accum)
+    (if (leaf? subtree)
+        (reverse accum)
+        (let ((lb (left-branch subtree))
+              (rb (right-branch subtree)))
+          (if (encodes-symbol? sym lb)
+              (walk-subtree lb (cons 0 accum))
+              (walk-subtree rb (cons 1 accum))))))
+  (if (encodes-symbol? sym tree)
+      (walk-subtree tree '())
+      (error "symbol not encoded by tree: " sym)))
 
 (define (encode message tree) (if (null? message)
                                   '()
                                   (append (encode-symbol (car message) tree)
                                           (encode (cdr message) tree))))
 
+(define encoded-message (encode decoded-message sample-tree))
+(println "(encode decoded-message sample-tree) ==> " encoded-message)
+(println "match? " (equal? sample-message encoded-message))
+
 ;;; Exercise 2.69
 
-(define (generate-huffman-tree pairs) (successive-merge (make-leaf-set pairs)))
+; (define (generate-huffman-tree pairs) (successive-merge (make-leaf-set pairs)))
 
 ;;; Exercise 2.70
 
